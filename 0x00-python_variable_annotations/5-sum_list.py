79863df5765659199d029326bf74d0c0b2c03641
Hovixen <orevaerijo@gmail.com>
2024-05-02 21:54:56 +0000

    task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -4,6 +4,7 @@ Type-annotated function sum_list
 """
 from typing import List
 
+
 def sum_list(n: List[float]) -> float:
     """ function takes a list as argument and returns their sum """
     return sum(n)
