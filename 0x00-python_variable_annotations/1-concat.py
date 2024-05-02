540fe40f337ee90bde592b673a2e683b4fb3ecde
Hovixen <orevaerijo@gmail.com>
2024-05-02 18:10:53 +0000

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Type-annotation function concat
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """ Function concatenates given strings """
+    return str1 + str2
