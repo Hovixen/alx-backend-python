6e95ac7c60de6b04e3b4a27c0a87cec2433f8fd0
Hovixen <orevaerijo@gmail.com>
2024-05-26 14:15:04 +0000

    task 0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,17 +8,14 @@ from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Tests the `access_nested_map` function."""
+    """ class to test access_nested_map function """
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-    ])
+        ("no_nest", {"a": 1}, ("a",), 1),
+        ("one_nest", {"a": {"b": 2}}, ("a",), {'b': 2}),
+        ("two_path", {"a": {"b": 2}}, ("a", "b"), 2)
+        ])
     def test_access_nested_map(
-            self,
-            nested_map: Dict,
-            path: Tuple[str],
-            expected: Union[Dict, int],
-            ) -> None:
-        """Tests `access_nested_map`'s output."""
+            self, name: str, nested_map: Dict,
+            path: Tuple[str], expected: Union[Dict, int]) -> None:
+        """ test assertEqual function """
         self.assertEqual(access_nested_map(nested_map, path), expected)
