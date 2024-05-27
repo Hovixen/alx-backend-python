eda06a59d80d8166e36a15312892e7f3f8860355
Hovixen <orevaerijo@gmail.com>
2024-05-27 22:10:37 +0000

    task 4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ test_client test case """
 import unittest
+from typing import Dict
 from unittest.mock import Mock, patch
 from parameterized import parameterized
 from client import GithubOrgClient
@@ -10,18 +11,19 @@ class TestGithubOrg(unittest.TestCase):
     """ class to test githuborgclient.org """
 
     @parameterized.expand([
-        ("google",),
-        ("abc",)
+        ("google", {"login": "google"}),
+        ("abc", {"login": "abc"})
         ])
     @patch('client.get_json')
-    def test_org(self, org_name: str, mock_get_json) -> None:
+    def test_org(self, org_name: str,
+                 response: Dict, mock_get_json) -> None:
         """ test test_org for the right output """
-        result_mocked = {"login": org_name}
-        mock_get_json.return_value = result_mocked
+
+        mock_get_json.return_value = response
 
         client = GithubOrgClient(org_name)
         result = client.org
 
-        self.assertEqual(result, result_mocked)
+        self.assertEqual(result, response)
         mock_get_json.assert_called_once_with(
                 "https://api.github.com/orgs/{}".format(org_name))
